66667645f92874c5c0f12b2b088c084956c811c4
Bruce Kirkpatrick <bruce@farbeyondcode.com>
2014-04-27 12:16:27 -0400

    removed db user from source code for mysql restore script

--- scripts/mysqlrestore.php
+++ scripts/mysqlrestore.php
@@ -4,13 +4,10 @@ usage: php /opt/jetendo/scripts/mysqlrestore.php
 */
 
 set_time_limit(20000);
-$BACKUP_DEST = get_cfg_var("jetendo_backup_path").'mysql/backup';
-$MYSQL_USER = 'fbcmysqlbackup'; 
-$MYSQL_PASSWD = '289nsahy1nHASYQN';
-
-$mp=$BACKUP_DEST;
-$user=$MYSQL_USER;
-$pw=$MYSQL_PASSWD;
+$mp=get_cfg_var("jetendo_backup_path").'mysql/backup';
+$host=get_cfg_var("jetendo_mysql_default_host");
+$user=get_cfg_var("jetendo_mysql_default_user");
+$pw=get_cfg_var("jetendo_mysql_default_password");
 echo "Restoring all files in ".$mp."\n\n";
 if ($handle = opendir($mp)) {
     while (false !== ($entry = readdir($handle))) {
@@ -24,13 +21,13 @@ if ($handle = opendir($mp)) {
 						$db=$entry;
 						$f=$mp."/".$entry."/".$entry2;
 						// this is safe, since it doesn't drop the user privileges
-						$cmd="echo \"DROP DATABASE ".$db.";\"|mysql -h 127.0.0.1 -u ".$user." --password=\"".$pw."\"";
+						$cmd="echo \"DROP DATABASE ".$db.";\"|mysql -h ".$host." -u ".$user." --password=\"".$pw."\"";
 						echo $cmd."\n\n";
 						$r=`$cmd`;
-						$cmd="echo \"CREATE DATABASE ".$db.";\"|mysql -h 127.0.0.1 -u ".$user." --password=\"".$pw."\"";
+						$cmd="echo \"CREATE DATABASE ".$db.";\"|mysql -h ".$host." -u ".$user." --password=\"".$pw."\"";
 						echo $cmd."\n\n";
 						$r=`$cmd`;
-						$cmd="mysql -h 127.0.0.1 -u ".$user." --password=\"".$pw."\" -D ".$db." < ".$f;
+						$cmd="mysql -h ".$host." -u ".$user." --password=\"".$pw."\" -D ".$db." < ".$f;
 						echo $cmd."\n\n";
 						$r=`$cmd`;
 						echo "Restored: ".$entry."/".$entry2." | ".$r."\n\n";
