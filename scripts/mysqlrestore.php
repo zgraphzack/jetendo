77bbe63ca026fdb06a83505d6cc3018f0b851273
Bruce Kirkpatrick <bruce@farbeyondcode.com>
2014-04-27 12:09:46 -0400

    updated mysql restore script to support uncompressed table sql files

--- scripts/mysqlrestore.php
+++ scripts/mysqlrestore.php
@@ -1,50 +1,46 @@
 <?php
 /*
-usage: php /opt/jetendo/scripts/mysqlrestore.php /zbackup/mysql/backup/mysql_backup_2013-07-10/ root rootpassword
+usage: php /opt/jetendo/scripts/mysqlrestore.php
 */
-//echo "path:".$argv[1]."\nuser:".$argv[2]."\npass:".$argv[3]."\n";
-//exit;
 
 set_time_limit(20000);
-if(count($argv)==0 || $argv[1] == ""){
-	echo "invalid parameters.";
-	exit;
-}
+$BACKUP_DEST = get_cfg_var("jetendo_backup_path").'mysql/backup';
+$MYSQL_USER = 'fbcmysqlbackup'; 
+$MYSQL_PASSWD = '289nsahy1nHASYQN';
 
-$mp=$argv[1];//"/home/mysqlbackup/";
-$user=$argv[2];//"root";
-$pw=$argv[3];//"123";
-chdir($mp);
+$mp=$BACKUP_DEST;
+$user=$MYSQL_USER;
+$pw=$MYSQL_PASSWD;
 echo "Restoring all files in ".$mp."\n\n";
 if ($handle = opendir($mp)) {
     while (false !== ($entry = readdir($handle))) {
-		echo $entry."\n";
-		if(strstr($entry, ".7z") !== FALSE && strstr($entry, "mysql.7z") === FALSE){
-			// sql file, do import:
-			$db=substr($entry, 0, strlen($entry)-7);
-			$f=$db.".sql";
-			if(!file_exists($mp.$f)){
-				$cmd="7za e ".$mp.$entry;
-				$r=`$cmd`;
-				echo $cmd."\n\n";
-				echo "Uncompressed: ".$entry." | ".$r."\n\n";
+		if($entry != "." && $entry != ".." && is_dir($mp."/".$entry)){
+			if ($handle2 = opendir($mp."/".$entry)) {
+				while (false !== ($entry2 = readdir($handle2))) {
+					echo $entry."/".$entry2."\n";
+					if(strstr($entry2, ".sql") !== FALSE){
+						// sql file, do import:
+						$r="";
+						$db=$entry;
+						$f=$mp."/".$entry."/".$entry2;
+						// this is safe, since it doesn't drop the user privileges
+						$cmd="echo \"DROP DATABASE ".$db.";\"|mysql -h 127.0.0.1 -u ".$user." --password=\"".$pw."\"";
+						echo $cmd."\n\n";
+						$r=`$cmd`;
+						$cmd="echo \"CREATE DATABASE ".$db.";\"|mysql -h 127.0.0.1 -u ".$user." --password=\"".$pw."\"";
+						echo $cmd."\n\n";
+						$r=`$cmd`;
+						$cmd="mysql -h 127.0.0.1 -u ".$user." --password=\"".$pw."\" -D ".$db." < ".$f;
+						echo $cmd."\n\n";
+						$r=`$cmd`;
+						echo "Restored: ".$entry."/".$entry2." | ".$r."\n\n";
+						//@unlink($f);
+					}
+				}
+				closedir($handle2);
 			}
-			$cmd="echo \"DROP DATABASE ".$db.";\"|mysql -h 127.0.0.1 -u ".$user." --password=\"".$pw."\"";
-			echo $cmd."\n\n";
-			$r=`$cmd`;
-			$cmd="echo \"CREATE DATABASE ".$db.";\"|mysql -h 127.0.0.1 -u ".$user." --password=\"".$pw."\"";
-			echo $cmd."\n\n";
-			$r=`$cmd`;
-			$cmd="mysql -h 127.0.0.1 -u ".$user." --password=\"".$pw."\" -D ".$db." < ".$f;
-			echo $cmd."\n\n";
-			$r=`$cmd`;
-			echo "Restored: ".$entry." | ".$r."\n\n";
-			@unlink($mp.$f);
 		}
-		//echo "done";
-		//exit;
-    }
-
-    closedir($handle);
+	}
+	closedir($handle);
 }
 ?>
