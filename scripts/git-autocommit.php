cec04007a622c4ed1bdff82bcc7f18ae7aff04ce
Bruce Kirkpatrick <bruce@farbeyondcode.com>
2016-05-17 15:23:25 -0400

    allowed git push to work

--- scripts/git-autocommit.php
+++ scripts/git-autocommit.php
@@ -1,10 +1,12 @@
 <?php
+set_time_limit(3000);
 // usage: php /var/jetendo-server/jetendo/scripts/git-autocommit.php "Site structure changed" "0"
 $rootPath=get_cfg_var("jetendo_root_path");
 if(!isset($argv) || count($argv) <= 2){
 	echo "\nYou must specify a commit message. I.e. \n\nphp ".$rootPath."scripts/git-autocommit.php \"Autocommit\"\n\n";
 	exit;
 }
+$sshKey=get_cfg_var("jetendo_git_ssh_key_path");
 
 $handle = opendir($rootPath."sites/");
 if($handle !== FALSE) {
@@ -21,6 +23,8 @@ if($handle !== FALSE) {
 			if(count($argv) == 3){
 				if($argv[2]=="1"){
 					$cmd="/usr/bin/git push -u origin --all";
+					$sshCMD="/usr/bin/ssh-agent bash -c '/usr/bin/ssh-add $sshKey; ".$cmd."'";
+					$s2=`$sshCMD`; 
 					echo $cmd."\n";
 					echo `$cmd`;
 				}
